822d93bd7e6711c203b83aca9d4f5c1941a1fef3
Ajay Dammalapati <ajaydammalapati30@gmail.com>
2026-04-08 16:03:23 +0530

    Added dynamic Weather background

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById('weatherForm').addEventListener('submit', async (e) => {
         weatherResult.textContent = "Please enter a city name.";
         return;
     }
-
+ 
     try {
         const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`;
         const response = await fetch(apiURL);
@@ -28,8 +28,17 @@ document.getElementById('weatherForm').addEventListener('submit', async (e) => {
             <p><strong>Weather:</strong> ${data.weather[0].main} (${data.weather[0].description})</p>
             <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
             <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
+            function changeBackground(weather){
+                if (weather === "Clear") {
+                document.body.style.backgroundImage = "url('images/sunny.jpg')";
+                } else if (weather === "Clouds") {
+                        document.body.style.backgroundImage = "url('images/cloudy.jpg')";
+                } else if (weather === "Rain") {
+                       document.body.style.backgroundImage = "url('images/rain.jpg')";
+              }
+          }
         `;
-    } catch (error) {
+        } catch (error) {
         weatherResult.textContent = "Error fetching weather data. Please try again.";
     }
 });
